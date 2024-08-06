59c62d6fdb457bd5c0334fc8d52d9c629aef3554
Abdulloh20080825 <abdullohkurbonov2008@gmail.com>
2024-08-06 20:58:59 +0500

    Change

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+	res.send('hello');
+});
+
 app.post('/login', async (req, res) => {
 	const { username, password } = req.body;
 	try {
