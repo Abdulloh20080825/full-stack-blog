ad098c629c99a3f7e65d3fe8fb041321fb8974c2
Abdulloh20080825 <abdullohkurbonov2008@gmail.com>
2024-08-06 21:52:46 +0500

    updaate

--- backend/middleware/isAuth.js
+++ backend/middleware/isAuth.js
@@ -1,15 +1,23 @@
 const jwt = require('jsonwebtoken');
 
 const authenticateToken = (req, res, next) => {
+	// Извлечение заголовка Authorization
 	const authHeader = req.headers['authorization'];
 	const token = authHeader && authHeader.split(' ')[1];
 
+	// Если токен отсутствует, вернуть статус 401
 	if (!token) {
-		return res.sendStatus(401);
+		return res.status(401).json({ message: 'Access token is missing' });
 	}
 
+	// Проверка токена с использованием секретного ключа
 	jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-		if (err) return res.sendStatus(401);
+		if (err) {
+			// Возвращаем статус 403 при ошибке токена (например, истекший срок действия)
+			return res.status(403).json({ message: 'Invalid or expired token' });
+		}
+
+		// Если токен верен, добавляем пользователя к запросу
 		req.user = user;
 		next();
 	});
